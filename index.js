e5dd965cefe6326fb1f4011333386fb5811247d8
Michael Schwartz <mykesx@gmail.com>
2020-12-09 13:15:38 -0800

    add disk info

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ process.title = process.env.TITLE || "sysinfo-microservice";
 
 const debug = require("debug")("sysinfo"),
   os = require("os"),
+  nodeDiskInfo = require("node-disk-info"),
   HostBase = require("microservice-core/HostBase");
 
 class SysInfoHost extends HostBase {
@@ -9,45 +10,56 @@ class SysInfoHost extends HostBase {
     const mqtt_host = process.env.MQTT_HOST || "mqtt";
     const topic = process.env.MQTT_TOPIC || "sysinfo";
 
-    super(mqtt_host, topic + '/' + os.hostname());
+    super(mqtt_host, topic + "/" + os.hostname());
+    this.retain = false;
+    this.maxLoadAvg = [];
     this.run();
   }
 
   async run() {
     for (;;) {
+      const disks = await nodeDiskInfo.getDiskInfo();
       this.state = { loadavg: os.loadavg() };
       this.state = { interfaces: os.networkInterfaces() };
       this.state = { cpus: os.cpus() };
-      this.state = { system: {
-        hostname: os.hostname(),
-        platform: os.platform(),
-        release: os.release(),
-        version: os.version(),
-        arch: os.arch(),
-      }};
+      this.state = {
+        system: {
+          hostname: os.hostname(),
+          platform: os.platform(),
+          release: os.release(),
+          version: os.version(),
+          arch: os.arch(),
+        },
+      };
       this.state = { uptime: os.uptime() };
-      this.state = { memory: { free: os.freemem(), total: os.totalmem()}};
+      this.state = { memory: { free: os.freemem(), total: os.totalmem() } };
+      this.state = { disks: disks };
 
       debug("loadavg", os.loadavg());
       debug("interfaces", os.networkInterfaces());
+      debug("disks", disks);
       debug("cpus", os.cpus());
       debug(
-        "hostname", os.hostname(), 
-        "platform", os.platform(), 
-        "release", os.release(),
-        "version", os.version(),
-        "arch", os.arch(), 
+        "hostname",
+        os.hostname(),
+        "platform",
+        os.platform(),
+        "release",
+        os.release(),
+        "version",
+        os.version(),
+        "arch",
+        os.arch()
       );
       debug("freemem", os.freemem(), "totalmem", os.totalmem());
       debug("uptime", os.uptime());
-      await this.wait(10000);
+      await this.wait(1000);
     }
   }
-};
+}
 
-function main() {
-  const host = new SysInfoHost();
+async function main() {
+    const host = new SysInfoHost();
 }
 
 main();
-
